85147d8d6c763f008a05a1d6449db61d560409e9
李翔 <lx70770@gmail.com>
2019-08-04 00:15:50 +0800

    test

--- db/mysql.js
+++ db/mysql.js
@@ -13,13 +13,14 @@ con.connect();
 // 统一执行 sql 的函数
 function exec(sql) {
   return new Promise((resolve, reject) => {
-    con.query(sql, (err, result) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(result);
-    });
+    // con.query(sql, (err, result) => {
+    //   if (err) {
+    //     reject(err);
+    //     return;
+    //   }
+    //   resolve(result);
+    // });
+    resolve({ code: 0, message: "1111111" });
   });
 }
 
